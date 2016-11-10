7d7739cd44ef176bbe23360082d224e20f060562
ghele <gheletus_raoul@yahoo.com>
2016-11-10 16:25:06 +0200

    AsyncWrites, AsyncStatus and ErrorHandling

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducers';
+import authors from './authorReducers';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
-  courses
+  courses,
+  authors,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
